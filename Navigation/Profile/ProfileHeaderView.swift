e64d83751413ae6721910b78a3837559f61860f9
Philip <rukorez@gmail.com>
2021-08-28 14:59:52 +0300

    Добавлено дополнительное задание, исправлены замечания основного задания

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -9,49 +9,72 @@ import UIKit
 
 class ProfileHeaderView: UIView {
     
-    var avatar = UIImageView()
+    var avatar: UIImageView = {
+        let avatar = UIImageView()
+        avatar.image = UIImage(named: "Ava")
+        avatar.layer.borderWidth = CGFloat(3)
+        avatar.layer.borderColor = CGColor(red: 255, green: 255, blue: 255, alpha: 10)
+        return avatar
+    }()
     
-    var photo = UIImage(named: "Ava")
+    var name: UILabel = {
+        let name = UILabel()
+        name.text = "Alco Dog"
+        name.textColor = .black
+        name.font = UIFont.boldSystemFont(ofSize: 18)
+        return name
+    }()
     
-    var name = UILabel()
+    var status: UITextView = {
+        let status = UITextView()
+        status.text = "Waiting for drink..."
+        status.textColor = .gray
+        status.font = status.font?.withSize(14)
+        status.backgroundColor = .lightGray
+        return status
+    }()
     
-    var status = UITextView()
+    var statusButton: UIButton = {
+        let statusButton = UIButton()
+        statusButton.backgroundColor = .systemBlue
+        statusButton.setTitle("Show Status", for: .normal)
+        statusButton.setTitleColor(UIColor.white, for: .normal)
+        statusButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        return statusButton
+    }()
     
-    var statusButton = UIButton()
+    var statusEdit: UITextField = {
+        let statusEdit = UITextField()
+        statusEdit.backgroundColor = .white
+        statusEdit.textColor = .black
+        statusEdit.font = statusEdit.font?.withSize(15)
+        statusEdit.layer.borderWidth = CGFloat(1)
+        statusEdit.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 10)
+        statusEdit.addTarget(self, action: #selector(statusTextChanged(_:)), for: .editingChanged)
+        return statusEdit
+    }()
+    
+    private var statusText: String?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         self.addSubview(avatar)
-        avatar.image = photo
-        avatar.clipsToBounds = true
-        avatar.layer.cornerRadius = avatar.frame.size.width/2
-        avatar.layer.borderWidth = CGFloat(3)
-        avatar.layer.borderColor = CGColor(red: 255, green: 255, blue: 255, alpha: 10)
-        
         self.addSubview(name)
-        name.text = "Alco Dog"
-        name.textColor = .black
-        name.font = UIFont.boldSystemFont(ofSize: 18)
-        
         self.addSubview(status)
-        status.text = "Waiting for drink..."
-        status.textColor = .gray
-        status.font = status.font?.withSize(14)
-        status.backgroundColor = .lightGray
-        
         self.addSubview(statusButton)
-        statusButton.backgroundColor = .systemBlue
-        statusButton.setTitle("Show Status", for: .normal)
-        statusButton.setTitleColor(UIColor.white, for: .normal)
-        statusButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
-        
+        self.addSubview(statusEdit)
     }
     
     @objc func buttonPressed() {
+        self.status.text = self.statusText
         if let currentStatus = self.status.text {
-            print(currentStatus)            
+            print(currentStatus)
         }
+    }
+    
+    @objc func statusTextChanged(_ textField: UITextField) {
+        self.statusText = textField.text
     }
     
     required init?(coder: NSCoder) {
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -22,11 +22,18 @@ class ProfileViewController: UIViewController {
     
     override func viewWillLayoutSubviews() {
         profileView.frame = self.view.frame
+        
         profileView.avatar.frame = CGRect(x: 16, y: self.view.safeAreaInsets.top + 16, width: 100, height: 100)
         profileView.avatar.clipsToBounds = true
         profileView.avatar.layer.cornerRadius = profileView.avatar.frame.size.width/2
-        profileView.name.frame = CGRect(x: self.view.center.x - 50, y: self.view.safeAreaInsets.top + 27, width: 200, height: 50)
-        profileView.status.frame = CGRect(x: self.view.center.x - 50, y: self.view.safeAreaInsets.top + 27 + self.profileView.name.bounds.height, width: 200, height: 50)
+        
+        profileView.name.frame = CGRect(x: self.view.center.x - 50, y: self.view.safeAreaInsets.top + 22, width: 200, height: 20)
+        
+        profileView.status.frame = CGRect(x: self.view.center.x - 50, y: self.view.safeAreaInsets.top + 22 + self.profileView.name.bounds.height + 10, width: 200, height: 25)
+        
+        profileView.statusEdit.frame = CGRect(x: self.view.center.x - 50, y: self.view.safeAreaInsets.top + 22 + self.profileView.name.bounds.height + 16 + self.profileView.status.bounds.height, width: 200, height: 40)
+        profileView.statusEdit.layer.cornerRadius = 10
+        
         profileView.statusButton.frame = CGRect(x: 16, y: self.view.safeAreaInsets.top + 16 + self.profileView.avatar.bounds.height + 16, width: self.view.frame.width - 32, height: 50)
         profileView.statusButton.layer.cornerRadius = 10
         profileView.statusButton.layer.shadowOffset.width = 4
@@ -34,6 +41,8 @@ class ProfileViewController: UIViewController {
         profileView.statusButton.layer.shadowRadius = 4
         profileView.statusButton.layer.shadowColor = CGColor(red: 0, green: 0, blue: 0, alpha: 10)
         profileView.statusButton.layer.shadowOpacity = 0.7
+        
+        
     }
     
 
