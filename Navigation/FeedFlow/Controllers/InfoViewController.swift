e737687f6cbf1b6749bfa6bb1d1a1ae0372b8a1f
rukorez <77271521+rukorez@users.noreply.github.com>
2022-08-30 00:40:53 +0300

    Сделано Задание 1

--- Navigation/FeedFlow/Controllers/InfoViewController.swift
+++ Navigation/FeedFlow/Controllers/InfoViewController.swift
@@ -11,29 +11,56 @@ final class InfoViewController: UIViewController {
     
     weak var coordinator: FeedCoordinator?
     
-     var buttonDelete: CustomButton = {
+    var buttonDelete: CustomButton = {
         let button = CustomButton(title: "Удалить все", titleColor: .white, backgroundColor: .clear)
         button.frame = CGRect(x: 100, y: 350, width: 200, height: 50)
         return button
     }()
+    
+    private var titleLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setViews()
+        fetchTitle()
     }
     
     private func setViews() {
         view.addSubview(buttonDelete)
+        view.addSubview(titleLabel)
+        titleLabel.frame = CGRect(x: 10, y: 20, width: view.bounds.width - 20, height: 30)
+        titleLabel.textAlignment = .center
         view.backgroundColor = .systemGray
         buttonDelete.onTap = { [ weak self ] in
             self?.tap()
         }
     }
     
-     func tap() {
+     private func tap() {
          coordinator?.showAlertModule(controller: self)
      }
+    
+    private func fetchTitle() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/todos/") else { return }
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            do {
+                if let data = data {
+                    let object = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.fragmentsAllowed)
+                    if let dictionary = object as? [[String:Any]] {
+                        let title = dictionary.randomElement()?["title"] as? String
+                        DispatchQueue.main.async {
+                            self.titleLabel.text = title
+                        }
+                    }
+                }
+                
+            } catch {
+                print(error)
+            }
+        }
+        task.resume()
+    }
 
 
 }
