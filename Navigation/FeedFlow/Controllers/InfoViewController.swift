bad0d0ae58dc50d3d8921e1680a6f656fbba8d55
rukorez <77271521+rukorez@users.noreply.github.com>
2022-08-30 18:58:37 +0300

    Выполнено Задание 2

--- Navigation/FeedFlow/Controllers/InfoViewController.swift
+++ Navigation/FeedFlow/Controllers/InfoViewController.swift
@@ -17,20 +17,34 @@ final class InfoViewController: UIViewController {
         return button
     }()
     
-    private var titleLabel = UILabel()
+    private var titleLabel: UILabel = {
+        var title = UILabel()
+        title.textAlignment = .center
+        title.numberOfLines = 0
+        return title
+    }()
+    
+    private var planetLabel:  UILabel = {
+        var planet = UILabel()
+        planet.textAlignment = .center
+        planet.numberOfLines = 0
+        return planet
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setViews()
         fetchTitle()
+        fetchOrbitalPeriiod()
     }
     
     private func setViews() {
         view.addSubview(buttonDelete)
         view.addSubview(titleLabel)
-        titleLabel.frame = CGRect(x: 10, y: 20, width: view.bounds.width - 20, height: 30)
-        titleLabel.textAlignment = .center
+        view.addSubview(planetLabel)
+        titleLabel.frame = CGRect(x: 10, y: 20, width: view.bounds.width - 20, height: 60)
+        planetLabel.frame = CGRect(x: 10, y: 20 + titleLabel.bounds.height + 20, width: view.bounds.width - 20, height: 60)
         view.backgroundColor = .systemGray
         buttonDelete.onTap = { [ weak self ] in
             self?.tap()
@@ -44,6 +58,9 @@ final class InfoViewController: UIViewController {
     private func fetchTitle() {
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/todos/") else { return }
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print(error)
+            }
             do {
                 if let data = data {
                     let object = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.fragmentsAllowed)
@@ -54,7 +71,26 @@ final class InfoViewController: UIViewController {
                         }
                     }
                 }
-                
+            } catch {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+    
+    private func fetchOrbitalPeriiod() {
+        guard let url = URL(string: "https://swapi.dev/api/planets/1") else { return }
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print(error)
+            }
+            do {
+                guard let data = data else { return }
+                let decoder = JSONDecoder()
+                let planetInstans = try decoder.decode(PlanetInstans.self, from: data)
+                DispatchQueue.main.async {
+                    self.planetLabel.text = planetInstans.orbitalPeriod
+                }
             } catch {
                 print(error)
             }
---
+++ Navigation/FeedFlow/Models/JSONModels/PlanetInstance.swift
@@ -0,0 +1,34 @@
+//
+//  PlanetInstance.swift
+//  Navigation
+//
+//  Created by Филипп Степанов on 30.08.2022.
+//
+
+import Foundation
+
+struct PlanetInstans: Decodable {
+    
+    var name: String
+    var rotationPeriod: String
+    var orbitalPeriod: String
+    var diameter: String
+    var climate: String
+    var gravity: String
+    var terrain: String
+    var surfaceWater: String
+    var population: String
+    var residents: [String]
+    var films: [String]
+    var created: String
+    var edited: String
+    var url: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name, diameter, climate, gravity, terrain, population, residents, films, created, edited, url
+        case rotationPeriod = "rotation_period"
+        case orbitalPeriod = "orbital_period"
+        case surfaceWater = "surface_water"
+    }
+    
+}
