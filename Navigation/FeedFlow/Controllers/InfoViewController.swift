50a5d3ce92a16d44b476e3f006324e5e627f99a0
rukorez <77271521+rukorez@users.noreply.github.com>
2022-08-30 23:12:02 +0300

    Сделано Задание 3

--- Navigation/FeedFlow/Controllers/InfoViewController.swift
+++ Navigation/FeedFlow/Controllers/InfoViewController.swift
@@ -11,25 +11,38 @@ final class InfoViewController: UIViewController {
     
     weak var coordinator: FeedCoordinator?
     
-    var buttonDelete: CustomButton = {
+    private var residentNames: [String] = []
+    
+    private var buttonDelete: CustomButton = {
         let button = CustomButton(title: "Удалить все", titleColor: .white, backgroundColor: .clear)
         button.frame = CGRect(x: 100, y: 350, width: 200, height: 50)
         return button
     }()
     
-    private var titleLabel: UILabel = {
+    private lazy var titleLabel: UILabel = {
         var title = UILabel()
         title.textAlignment = .center
         title.numberOfLines = 0
         return title
     }()
     
-    private var planetLabel:  UILabel = {
+    private lazy var planetLabel:  UILabel = {
         var planet = UILabel()
         planet.textAlignment = .center
         planet.numberOfLines = 0
         return planet
     }()
+    
+    private lazy var tableView: UITableView = {
+        var tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.backgroundColor = .white
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,14 +50,18 @@ final class InfoViewController: UIViewController {
         setViews()
         fetchTitle()
         fetchOrbitalPeriiod()
+        fetchResidentNames()
     }
     
     private func setViews() {
         view.addSubview(buttonDelete)
         view.addSubview(titleLabel)
         view.addSubview(planetLabel)
+        view.addSubview(tableView)
         titleLabel.frame = CGRect(x: 10, y: 20, width: view.bounds.width - 20, height: 60)
         planetLabel.frame = CGRect(x: 10, y: 20 + titleLabel.bounds.height + 20, width: view.bounds.width - 20, height: 60)
+        tableView.frame = CGRect(x: 0, y: 350 + buttonDelete.bounds.height + 10, width: view.bounds.width,
+                                 height: view.bounds.height - (350 + buttonDelete.bounds.height + 10))
         view.backgroundColor = .systemGray
         buttonDelete.onTap = { [ weak self ] in
             self?.tap()
@@ -54,6 +71,12 @@ final class InfoViewController: UIViewController {
      private func tap() {
          coordinator?.showAlertModule(controller: self)
      }
+
+}
+
+// Реализация парсинга JSON
+
+extension InfoViewController {
     
     private func fetchTitle() {
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/todos/") else { return }
@@ -89,7 +112,7 @@ final class InfoViewController: UIViewController {
                 let decoder = JSONDecoder()
                 let planetInstans = try decoder.decode(PlanetInstans.self, from: data)
                 DispatchQueue.main.async {
-                    self.planetLabel.text = planetInstans.orbitalPeriod
+                    self.planetLabel.text = "The planet \(planetInstans.name) has orbital period is \(planetInstans.orbitalPeriod)"
                 }
             } catch {
                 print(error)
@@ -97,6 +120,54 @@ final class InfoViewController: UIViewController {
         }
         task.resume()
     }
+    
+    private func fetchResidentNames() {
+        guard let url = URL(string: "https://swapi.dev/api/planets/1") else { return }
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print(error)
+            }
+            do {
+                guard let data = data else { return }
+                let decoder = JSONDecoder()
+                let planetInstans = try decoder.decode(PlanetInstans.self, from: data)
+                planetInstans.residents.forEach { resident in
+                    let residentUrl = URL(string: resident)
+                    guard let residentUrl = residentUrl else { return }
+                    let task = URLSession.shared.dataTask(with: residentUrl) { data, response, error in
+                        do {
+                            guard let data = data else { return }
+                            let resident = try JSONDecoder().decode(Resident.self, from: data)
+                            self.residentNames.append(resident.name)
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        } catch {
+                            print(error)
+                        }
+                    }
+                    task.resume()
+                }
+            } catch {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+}
 
+// Настройка UITableView
 
+extension InfoViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return residentNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = residentNames[indexPath.row]
+        return cell
+    }
+    
 }
--- Navigation/FeedFlow/Models/JSONModels/PlanetInstance.swift
+++ Navigation/FeedFlow/Models/JSONModels/PlanetInstance.swift
@@ -32,3 +32,31 @@ struct PlanetInstans: Decodable {
     }
     
 }
+
+struct Resident: Decodable {
+    var name: String
+    var height: String
+    var mass: String
+    var hairColor: String
+    var skinColor: String
+    var eyeColor: String
+    var birthYear: String
+    var gender: String
+    var homeworld: String
+    var films: [String]
+    var species: [String]
+    var vehicles: [String]
+    var starships: [String]
+    var created: String
+    var edited: String
+    var url: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name, height, mass, gender, homeworld, films, species, vehicles, starships, created, edited, url
+        case hairColor = "hair_color"
+        case skinColor = "skin_color"
+        case eyeColor = "eye_color"
+        case birthYear = "birth_year"
+    }
+    
+}
